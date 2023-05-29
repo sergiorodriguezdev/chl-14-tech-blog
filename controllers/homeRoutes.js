08cec5f52039d8bff1742f9b8f441d7e8edab6c4
Sergio <119548442+sergiorodriguezdev@users.noreply.github.com>
2023-05-29 00:25:29 -0600

    Add home routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,69 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+      order: [["createdOn", "DESC"]],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render("homepage", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+router.get("/dashboard", async (req, res) => {
+  if (req.session.logged_in) {
+    try {
+      const postData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ["name"],
+          },
+        ],
+        where: {
+          userId: req.session.user_id
+        },
+        order: [["createdOn", "DESC"]],
+      });
+
+      const posts = postData.map((post) => post.get({ plain: true }));
+
+      res.render("dashboard", {
+        posts,
+        logged_in: req.session.logged_in,
+      });
+      return;
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
+  }
+
+  res.redirect("/login");
+});
+
+router.get("/login", async (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
